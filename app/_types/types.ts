5d79852c097d143090c0fd403bddf1aa69c1e2aa
s072062-tech <s072062@gmail.com>
2026-09-05 21:18:02 +0900

    Category型 追加

--- app/_types/types.ts
+++ app/_types/types.ts
@@ -8,11 +8,16 @@ export type Post = {
   content: string,
 };
 
+type Category = {
+  id: string,
+  name: string,
+};
+
 export type MicroCmsPost = {
   id: string,
   title: string,
   content: string,
   createdAt: string,
-  categories: { id: string; name: string }[],
+  categories: Category[],
   thumbnail: { url: string; height: number; width: number },
 };
