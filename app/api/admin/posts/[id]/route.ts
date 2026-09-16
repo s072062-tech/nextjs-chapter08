72c33c73487f8c56953f2770c50f70d72cda4157
s072062-tech <s072062@gmail.com>
2026-09-17 00:39:40 +0900

    API実装

---
+++ app/api/admin/posts/[id]/route.ts
@@ -0,0 +1,112 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/app/_libs/prisma";
+import type { PropsParams } from "@/app/_types/types";
+import type { GetPostsIdResponse } from "@/app/api/posts/[id]/route";
+
+// 記事更新リクエスト
+export type UpdatePostRequestBody = {
+  title: string,
+  content: string,
+  categories: { id: number }[],
+  thumbnailUrl: string,
+};
+
+export const GET = async (_request: Request, {params}: PropsParams) => {
+  const {id} = await params;
+  const postId = Number(id);
+
+  if (Number.isNaN(postId)) {
+    return NextResponse.json({ message: "不正なIDです" }, { status: 400 });
+  }
+
+  try {
+    // Idを指定してPostを取得
+    const post = await prisma.post.findUnique({
+      where: { id: postId },
+      include: {
+        postCategories: {
+          include: {
+            category: {
+              select: {
+                id: true,
+                name: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    if (!post) {
+      return NextResponse.json({ message: "記事が見つかりません" }, { status: 404 },);
+    }
+
+    // レスポンス
+    return NextResponse.json<GetPostsIdResponse>({ post }, { status: 200 });
+
+  // エラー
+  } catch (error) {
+    if (error instanceof Error)
+      return NextResponse.json({ message: error.message }, { status: 400 });
+  }
+};
+
+export const PUT = async (request: Request, {params}: PropsParams) => {
+  const {id} = await params;
+  const postId = Number(id);
+
+  if (Number.isNaN(postId)) {
+    return NextResponse.json({ message: "不正なIDです" }, { status: 400 });
+  }
+
+  const body = (await request.json()) as UpdatePostRequestBody;
+  const { title, content, categories, thumbnailUrl } = body;
+
+  try {
+    // 指定されたPostを更新
+    await prisma.post.update({
+      where: { id: postId },
+      data: {
+        title,
+        content,
+        thumbnailUrl,
+        postCategories: {
+          deleteMany: {},
+          create: categories.map((category) => ({ categoryId: category.id })),
+        },
+      },
+    });
+
+    // レスポンス
+    return NextResponse.json({ message: "OK" }, { status: 200 });
+
+  // エラー
+  } catch (error) {
+    if (error instanceof Error)
+      return NextResponse.json({ message: error.message }, { status: 400 });
+  }
+};
+
+export const DELETE = async (_request: Request, {params}: PropsParams) => {
+  const {id} = await params;
+  const postId = Number(id);
+
+  if (Number.isNaN(postId)) {
+    return NextResponse.json({ message: "不正なIDです" }, { status: 400 });
+  }
+
+  try {
+    // Idを指定してPostを削除
+    await prisma.post.delete({
+      where: { id: postId },
+    });
+
+    // レスポンス
+    return NextResponse.json({ message: "OK" }, { status: 200 });
+
+  // エラー
+  } catch (error) {
+    if (error instanceof Error)
+      return NextResponse.json({ message: error.message }, { status: 400 });
+  }
+};
